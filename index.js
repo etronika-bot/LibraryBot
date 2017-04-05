04ac327dc9894f3954bba772aa7f76c81d464a57
Martins Kalis <martins@kalis.lv>
2017-04-05 13:30:00 +0300

    Added library choice.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var u = require('./utility.js');
+var libs = require('./libs.json');
 
 //=========================================================
 // Bot Setup
@@ -30,26 +31,39 @@ intents
     .matches(/autor/i, '/author')
     .matches(/grāmat/i, '/book')
     .matches(/atbild/i, '/qna')
+    .matches(/annotate-/i, '/annotate')
+    .matches(/mainīt bibliotēku/i, '/chooselib')
+    .matches(/libid-/, '/libid')
     .onDefault('/default');
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/default', session => {
-    let msg = new builder.Message(session)
-        .addAttachment(
-            new builder.HeroCard(session)
-                .title('Ko Tu vēlies meklēt?')
-                .buttons(
-                    ["Autoru", "Grāmatu", "Atbildes"].map(
-                        b => builder.CardAction.postBack(session, b, b)
+bot.dialog('/default', [
+    (session, args, next) => {
+        console.log(session.userData);
+        if (!session.userData.lib) {
+            session.beginDialog('/chooselib');
+        } else {
+            next();
+        }
+    },
+    session => {
+        let msg = new builder.Message(session)
+            .addAttachment(
+                new builder.HeroCard(session)
+                    .title('Ko Tu vēlies darīt?')
+                    .buttons(
+                        ["Meklēt autoru", "Meklēt grāmatu", "Uzdot jautājumu", "Mainīt bibliotēku"].map(
+                            b => builder.CardAction.postBack(session, b, b)
+                        )
                     )
-                )
-        );
-    session.send(msg);
-    session.endDialog();
-});
+            );
+        session.send(msg);
+        session.endDialog();
+    }
+]);
 
 bot.dialog('/book', [
     session => {
@@ -76,3 +90,34 @@ bot.dialog('/qna', session => {
     session.send('Te meklēsim atbildes uz jautājumiem brīvā formātā.')
     session.endDialog();
 });
+
+bot.dialog('/annotate', session => {
+    let t = session.message.text;
+    u.get_book(t.slice(t.indexOf('-') + 1))
+        .then(b => {session.send(b.annotation ? b.annotation : 'Diemžēl plašāka apraksta nav.');});
+    session.endDialog();
+});
+
+bot.dialog('/chooselib', [
+    session => {
+        builder.Prompts.text(session, 'Kurā bibliotēkā Tev būtu visērtāk saņemt grāmatas?');
+    },
+    (session, res) => {
+        let carousel = u.lib_carousel(session, u.get_lib_options(res.response));
+        let msg = new builder.Message(session);
+        msg.attachmentLayout(builder.AttachmentLayout.carousel);
+        msg.attachments(carousel);
+        session.send(msg);
+        session.endDialog();
+    }
+]);
+
+bot.dialog('/libid', [
+    session => {
+        let t = session.message.text;
+        session.userData.lib = t.slice(t.indexOf('-') + 1);
+        let lib = libs.filter(lib => (session.userData.lib == lib.id))[0];
+        session.send("Izvēlēta bibliotēka: " + lib.name);
+        session.endDialog();
+    }
+]);
--- utility.js
+++ utility.js
@@ -2,10 +2,57 @@
 var jsdom = require('jsdom');
 var request = require('request');
 var builder = require('botbuilder');
+var Fuse = require('fuse.js');
+var libs = require('./libs.json');
+var store = require('azure-storage');
+
+
+var get_book = function(id) {
+    let bookurl = 'https://jelgava.biblioteka.lv/AliseRestApi/api/BibliographicRecords/';
+    let thumburl = 'https://jelgava.biblioteka.lv/Alise/bookthumbnail.axd?version=070217_015954&maxHeight=250&id='; //&maxWidth=185
+    return new Promise((resolve, reject) => {
+        request({
+            url: bookurl + id,
+            json: true
+        }, (err, res, body) => {
+            if (!err && res.statusCode === 200) {
+                if (!body.HasCopies) {
+                    reject('Kopijas nav pieejamas');
+                }
+                let book = {
+                    id: body.Id,
+                    title: body.Title,
+                    author: body.Author,
+                    isbn: body.ISBN,
+                    year: body.Year,
+                    annotation: body.Annotation
+                }
+                resolve(book);
+            }
+        });
+    }).then(book => {
+        return new Promise((resolve) => {
+            request({
+                url: thumburl + id
+            },
+            (err, res, body) => {
+                if (!err && res.statusCode === 200) {
+                    book.img = thumburl + book.id;
+                }
+                else {
+                    book.img = "http://www.library.psychol.cam.ac.uk/images/GenericBook/image";
+                }
+                resolve(book);
+            });
+        });
+    })
+    .catch(err => {
+        return Promise.resolve(false);
+    });
+}
 
 var search_book = function (s) {
     let url = 'https://jelgava.biblioteka.lv/Alise/lv/advancedsearch.aspx?crit0=name&op0=%25LIKE%25&val0=';
-    let bookurl = 'https://jelgava.biblioteka.lv/AliseRestApi/api/BibliographicRecords/';
     return new Promise((resolve, reject) => {
         jsdom.env(
             url + s,
@@ -22,47 +69,55 @@ var search_book = function (s) {
                 while ((result = re.exec(d.getElementById('_SearchResult').innerHTML)) !== null) {
                     ids.push(result[1]);
                 }
-                Promise.all(
-                    ids.map(id => {
-                        return new Promise((resolve, reject) => {
-                            request({
-                                url: bookurl + id,
-                                json: true
-                            }, (err, res, body) => {
-                                if (!err && res.statusCode === 200) {
-                                    let book = {
-                                        id: body.Id,
-                                        title: body.Title,
-                                        author: body.Author,
-                                        isbn: body.ISBN,
-                                        year: body.Year,
-                                        annotation: body.Annotation
-                                    }
-                                    resolve(book);
-                                }
-                            });
-                        });
-                    })
-                ).then(books => {resolve(books);});
+                Promise.all(ids.map(get_book)).then(books => {
+                    resolve(books.filter(b => b));
+                });
             }
         );
     });
 };
 
 var book_carousel = function(session, books) {
-    console.log(books);
+    let bookurl = 'https://jelgava.biblioteka.lv/Alise/lv/book.aspx/bibliographic?id=';
     return books.map(book => {
-        console.log(book.title);
+        let buttons = [builder.CardAction.openUrl(session, bookurl + book.id, "Atvērt bibliotēkas lapu")];
+        if (book.annotation) {buttons.push(builder.CardAction.postBack(session, 'annotate-' + book.id, "Vairāk"));}
         return new builder.HeroCard(session)
             .title(book.title)
             .text(book.author ? book.author : '-')
             .images([
-                builder.CardImage.create(session, "http://www.library.psychol.cam.ac.uk/images/GenericBook/image")
-            ]);
-    })
+                builder.CardImage.create(session, book.img)
+            ])
+            .buttons(buttons);
+    });
+}
+
+var get_lib_options = function(s) {
+    let opts = {
+        keys: ['name'],
+        threshold: 0.4,
+    }
+    let fuse = new Fuse(libs, opts);
+    let result = fuse.search(s);
+    return(result.length > 10 ? result.slice(0,10) : result);
+}
+
+var lib_carousel = function(session, libs) {
+    return libs.map(lib => {
+        let buttons = [
+            builder.CardAction.openUrl(session, lib.home_page_url, "Atvērt bibliotēkas lapu"),
+            builder.CardAction.imBack(session, 'libid-' + lib.id, "Izvēlēties šo")
+        ];
+        return new builder.HeroCard(session)
+            .title(lib.name)
+            .buttons(buttons);
+    });
 }
 
 module.exports = {
     search_book: search_book,
-    book_carousel: book_carousel
+    book_carousel: book_carousel,
+    get_book: get_book,
+    get_lib_options: get_lib_options,
+    lib_carousel: lib_carousel
 };
